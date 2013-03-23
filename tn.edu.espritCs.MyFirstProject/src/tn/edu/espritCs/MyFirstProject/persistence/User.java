6a13f2dd58fde2f33f5b10f38c68611add83b3f8
Cyborg <Cyborg@172.16.99.7>
2013-03-23 16:57:49 +0100

    Signed-off-by: Cyborg <Cyborg@172.16.99.7>

--- tn.edu.espritCs.MyFirstProject/src/tn/edu/espritCs/MyFirstProject/persistence/User.java
+++ tn.edu.espritCs.MyFirstProject/src/tn/edu/espritCs/MyFirstProject/persistence/User.java
@@ -2,4 +2,12 @@ package tn.edu.espritCs.MyFirstProject.persistence;
 
 public class User {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("My first Program");
+	}
 }
